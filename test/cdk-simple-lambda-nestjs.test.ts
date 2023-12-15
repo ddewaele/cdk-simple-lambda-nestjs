59ac307a651ae26aadf122eb291bd99fa26c65cd
ddewaele <ddewaele@gmail.com>
2023-12-15 07:02:54 +0100

    Initial commit

---
+++ test/cdk-simple-lambda-nestjs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkSimpleLambdaNestjs from '../lib/cdk-simple-lambda-nestjs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-simple-lambda-nestjs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkSimpleLambdaNestjs.CdkSimpleLambdaNestjsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
